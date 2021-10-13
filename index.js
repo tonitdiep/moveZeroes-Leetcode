75a1946d34e22e806be86d98ebb72ac64ee95461
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2021-10-13 11:07:11 +0000

    update with solution

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-Leetcode 283 Move Zeroes
-Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Leetcode 283 Move Zeroes
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-Note that you must do this in-place without making a copy of the array.
+// Note that you must do this in-place without making a copy of the array.
 
  
 
-Example 1:
+// Example 1:
 
-Input: nums = [0,1,0,3,12]
-Output: [1,3,12,0,0]
-Example 2:
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
 
-Input: nums = [0]
-Output: [0]
+// Input: nums = [0]
+// Output: [0]
  
 
-Constraints:
+// Constraints:
 
-1 <= nums.length <= 104
--231 <= nums[i] <= 231 - 1
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  
-}
+
+    if(nums.length==0 || nums.length==1)
+    return;
+
+    let left =0;
+    let right=0;
+    let temp;
+    while(right<nums.length) {
+        if(nums[right] ==0) {
+            right++;
+        }
+        else {
+            temp = nums[right];
+            nums[right]=nums[left]
+            nums[left]=temp;
+            left++;
+            right++;
+        }
+
+    }
+};
